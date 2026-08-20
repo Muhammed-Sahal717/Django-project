880940e7f11cd30bd9c8ae53ba3527f322d14554
muhammed sahal <muhammedsahalap717@gmail.com>
2026-08-20 20:55:20 +0530

    Implement dynamic content in home view and update HTML template

--- core/views.py
+++ core/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, 'home.html')
+    context = {
+        'app_name': 'My Awesome Django App',
+        'subtitle': 'A minimal, lightning-fast foundation for building modern web applications.',
+        'features': ['Fast', 'Secure', 'Scalable', 'Modern'],
+    }
+    return render(request, 'home.html', context)
